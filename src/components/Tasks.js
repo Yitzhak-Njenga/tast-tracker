e0f6b15410bcb76aa618cada589341392d6e242f
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-06-10 17:50:04 +0300

    added delete function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,13 @@
 import Task  from "./Task"
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks,onDelete}) => {
     
   return (
     <>
     {/* getting text for each task with a specic id */}
-        {tasks.map((task) =>(<Task  key={task.id} task={task}/>))}
-
+        {tasks.map((task) =>(<Task  key={task.id} task={task}
+        onDelete={onDelete}
+        />))}
+        
     </>
   )
 }
